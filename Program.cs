93d36d57b552fc51444ba4f2271c7e76e2f09d5e
Ibrahim Al Busaidi <i.s.busaidi@gmail.com>
2024-09-04 11:55:10 +0400

    Updated the path of the books information file.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace BasicLibrary
     internal class Program
     {
         static List<(string BName, string BAuthor, int ID)> Books = new List<(string BName, string BAuthor, int ID)>();
-        static string filePath = "C:\\Users\\Karim\\Downloads\\OutSystem_Course\\GitHubRepos\\Data\\lib.txt";
+        static string filePath = "C:\\Users\\Lenovo\\Desktop\\Ibrahim_Projects\\LibrarySystemFiles\\LibraryBooks.txt";
 
         static void Main(string[] args)
         {
